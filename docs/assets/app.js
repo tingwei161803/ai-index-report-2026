5fbbfbfdbd3a9de251125d97fa0f57ff63b29b47
Ting Wei Chang <75680689+tingwei161803@users.noreply.github.com>
2026-06-22 01:09:25 +0800

    Make English the default page language (#2)

--- docs/assets/app.js
+++ docs/assets/app.js
@@ -65,7 +65,7 @@
 
   /* ---------- global state ---------- */
   var state = {
-    lang:  lsGet("lang")  || "zh",       // default language: zh
+    lang:  lsGet("lang")  || "en",       // default language: zh
     theme: lsGet("theme") || "light"
   };
 
